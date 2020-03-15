96231d64543d58b4a2edde5f23ca9f4d72fb8723
Булгаков Роман <deregodub@gmail.com>
2020-03-15 18:17:21 +0700

    added transition between main and login pages + check on login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -6,6 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser,link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser,link)
@@ -13,5 +16,5 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
     login_link.click()
